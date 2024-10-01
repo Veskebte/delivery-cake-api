4c005f2afd64d9ac1a0c7a5c7508f8871f677b24
Veskebte <nathasyapernidyaashari_2327250063@mhs.mdp.ac.id>
2024-10-01 12:35:43 +0700

    Revision Final

--- api/routes/api.php
+++ api/routes/api.php
@@ -10,19 +10,23 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/cakes', [CakeController::class, 'index']);
+    Route::post('/cakes', [CakeController::class, 'store']);
+    Route::get('/cakes/{id}', [CakeController::class, 'show']);
+    Route::put('/cakes/{id}', [CakeController::class, 'update']);
+    Route::delete('/cakes/{id}', [CakeController::class, 'destroy']);
+
+    Route::get('/orders', [OrderController::class, 'index']);
+    Route::post('/orders', [OrderController::class, 'store']);
+    Route::get('/orders/{id}', [OrderController::class, 'show']);
+    Route::put('/orders/{id}', [OrderController::class, 'update']);
+    Route::delete('/orders/{id}', [OrderController::class, 'destroy']);
+
+    Route::patch('orders/{id}/status', [OrderController::class, 'updateStatus']);
+});
+
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::get('/cakes', [CakeController::class, 'index']);
-Route::post('/cakes', [CakeController::class, 'store']);
-Route::get('/cakes/{id}', [CakeController::class, 'show']);
-Route::put('/cakes/{id}', [CakeController::class, 'update']);
-Route::delete('/cakes/{id}', [CakeController::class, 'destroy']);
 
-Route::get('/orders', [OrderController::class, 'index']);
-Route::post('/orders', [OrderController::class, 'store']);
-Route::get('/orders/{id}', [OrderController::class, 'show']);
-Route::put('/orders/{id}', [OrderController::class, 'update']);
-Route::delete('/orders/{id}', [OrderController::class, 'destroy']);
-
-Route::patch('orders/{id}/status', [OrderController::class, 'updateStatus']);
